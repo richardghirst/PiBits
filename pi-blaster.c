3afe3ae90a11a592a6ecada3d6473f07b86e9a58
ThomasMcVay <contact@thomas-mcvay.info>
2016-12-01 22:59:26 -0800

    Testing2

--- pi-blaster.c
+++ pi-blaster.c
@@ -871,13 +871,13 @@ go_go_go(void)
 			continue;
 		dprintf("[%d]%s", n, lineptr);
 		if (strcmp(lineptr, "debug_regs\n")) {
-			fprintf(stderr, "debug_dump_hw", lineptr);
+			printf("debug_dump_hw");
 			debug_dump_hw();
 		} else if (strcmp(lineptr, "debug_samples\n")) {
-			fprintf(stderr, "debug_dump_samples", lineptr);
+			printf("debug_dump_samples");
 			debug_dump_samples();
 		} else if (sscanf(lineptr, "release %d%c", &servo, &nl) == 2 && nl == '\n') {
-			fprintf(stderr, "debug_dump_samples", lineptr);
+			printf("release_pwm");
 			//release_pwm(servo);
 		} else if (sscanf(lineptr, "*=%f%c", &value, &nl) == 2 && nl == '\n') {
 			if (value < 0 || value > 1) {
