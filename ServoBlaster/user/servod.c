c1bd17bff0184b9f8ba41b4158a1b9591908273b
Joey232 <56601488+Joey232@users.noreply.github.com>
2020-12-21 13:40:48 -0500

    Update servod.c
    Fix for "Cannot parse the hardware name string" issue.

--- ServoBlaster/user/servod.c
+++ ServoBlaster/user/servod.c
@@ -972,7 +972,7 @@ get_model_and_revision(void)
 
 	if (strstr(modelstr, "BCM2708"))
 		board_model = 1;
-	else if (strstr(modelstr, "BCM2709") || strstr(modelstr, "BCM2835"))
+	else if (strstr(modelstr, "BCM2709") || strstr(modelstr, "BCM2835") || strstr(modelstr, "BCM2711"))
 		board_model = 2;
 	else
 		fatal("servod: Cannot parse the hardware name string\n");
